f1455b0d7bd1a0b3bec5fd8cb638128d949a6e9a
GregDoming <greg.domingue1@gmail.com>
2019-02-12 02:06:09 -0800

    node_modules not updating correctly

--- deployment/scripts/lambda_deploy.js
+++ deployment/scripts/lambda_deploy.js
@@ -2,27 +2,31 @@ const AWS = require('aws-sdk');
 const AdmZip = require('adm-zip');
 const dotenv = require('dotenv');
 
-const lambda = new AWS.Lambda({
-  region: 'us-west-2',
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-});
 
 const zip = new AdmZip();
-
+//adds the folder and all the colders children to the zip que
 zip.addLocalFile('lambda/index.js');
+zip.addLocalFile('lambda/package.json');
+zip.addLocalFolder('lambda/node_modules');
+//zips all files in the zip que
 zip.writeZip('deployment/lambda_zip/nameAnalyzer.zip');
 
-const bufferedZip = zip.toBuffer();
+// const bufferedZip = zip.toBuffer();
 
-const updateFunctionParams = {
-  FunctionName: 'nameAnalyzer', /* required */
-  DryRun: false,
-  Publish: false,
-  ZipFile: bufferedZip,
-};
+// const lambda = new AWS.Lambda({
+//     region: 'us-west-2',
+//     accessKeyId: process.env.AWS_ACCESS_KEY,
+//     secretAccessKey: process.env.AWS_SECRET_KEY,
+// });
 
-lambda.updateFunctionCode(updateFunctionParams, (err, data) => {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
+// const updateFunctionParams = {
+//   FunctionName: 'nameAnalyzer', /* required */
+//   DryRun: false,
+//   Publish: false,
+//   ZipFile: bufferedZip,
+// };
+
+// lambda.updateFunctionCode(updateFunctionParams, (err, data) => {
+//   if (err) console.log(err, err.stack); // an error occurred
+//   else     console.log(data);           // successful response
+// });
